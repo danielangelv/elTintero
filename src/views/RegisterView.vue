004983174d5da55b3f027a07554c5ff959b3a1ae
UTP <utp@utp.local>
2022-10-19 19:40:52 -0500

    rehacer registro

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,92 +1,164 @@
 <template>
-  <div class="border-solid border-lime-400 border-2 m-10 bg-white">
-    <h1>Registrate</h1>
-    <hr />
-    <div class="grid grid-cols-2 gap-4">
-      <div class="grid grid-rows-2 p-2">
-        <label for="Name" >Nombre Completo: </label>
-        <input type="text" class="w-full" id="Name" />
-      </div>
-      <div class="grid grid-rows-2 p-2">
-        <label for="Identificacion" >Identificacion</label>
-        <input type="text" class="w-full" id="Identificacion" />
-      </div>
-      <div class="grid grid-rows-2 p-2">
-        <label for="User" >Nombre de Usuario</label>
-        <input type="text" class="w-full" id="User" />
-      </div>
-      <div class="grid grid-rows-2 p-2">
-        <label for="Clave">Contraseña</label>
-        <input type="password" class="w-full" id="Clave" />
-      </div>
-      <div class="grid grid-rows-2 p-2">
-        <label for="Email" >Email</label>
-        <input type="email" class="w-full" id="Email" />
-      </div>
-      <div class="grid grid-rows-2 p-2">
-        <label for="PhoneNumber" >Numero de Telefono</label>
-        <input type="text" class="w-full" id="PhoneNumber" />
-      </div>
-      <div class="grid grid-rows-2 p-2">
-        <label for="MailingAddress" >Dirección de Correspondencia</label>
-        <input type="text" class="w-full" id="MailingAddress" />
-      </div>
-      <div class="grid grid-rows-2 p-2">
-        <label for="BirthDate" >Fecha de Nacimiento</label>
-        <input type="date" class="w-full" id="BirthDate" />
-      </div>
-      <div class="grid grid-rows-2 p-2">
-        <label  for="BirthPlace">Lugar de Nacimiento</label>
-        <input type="text" class="w-full" id="BirthPlace" />
-      </div>
-      <div class="grid grid-rows-2 p-2">
-        <p for="Gender">Genero</p>
-        <div class="grid grid-cols-3 p-2">
-          <div>
-            <input type="radio" value="masculino" id="radio-masculino" name="genero" />
-            <label for="radio-masculino">Masculino</label>
+  <div class="bg-gray-100 mx-auto max-w-6xl bg-white py-5 px-5 lg:px-10 shadow-xl mb-10">
+    <div class="relative rounded-xl overflow-auto p-3">
+      <img src="../assets/logo.png" class="text-center rounded-lg overflow-hidden w-56 sm:w-20 mx-auto">
+    </div>
+    <form>
+      <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+        <div class="-mx-3 md:flex mb-6">
+          <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="company">
+              Nombre completo*
+            </label>
+            <input class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1" style="color:black;" id="company" type="text" placeholder="Ej: Pepito Perez Marques">
+            <div>
+              <span class="text-red-500 text-xs italic">
+                Campo obligatorio.
+              </span>
+            </div>
           </div>
-          <div>
-            <input type="radio" value="femenino" id="radio-femenino" name="genero" />
-            <label for="radio-femenino">Femenino</label>
-          </div>
-          <div>
-            <input type="radio" value="otro" id="radio-otro" name="genero"/>
-            <label for="radio-otro">Otro</label>
+          <div class="md:w-1/2 px-3">
+            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="iden">
+              Identificación*
+            </label>
+            <input class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1" style="color:black" id="iden" type="number" placeholder="Ej: 1088559554">
+            <div>
+              <span class="text-red-500 text-xs italic">
+                Campo obligatorio.
+              </span>
+            </div>
           </div>
         </div>
+        <div class="-mx-3 md:flex mb-6">
+          <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="company">
+              Usuario*
+            </label>
+            <input class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1" style="color:black;" id="company" type="text" placeholder="Ej: pepito_perez1">
+            <div>
+              <span class="text-red-500 text-xs italic">
+                Campo obligatorio.
+              </span>
+            </div>
+          </div>
+          <div class="md:w-1/2 px-3">
+            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="title">
+              Email*
+            </label>
+            <input class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1" style="color:black" id="title" type="email" placeholder="Ej: pepito@eltintero.com">
+            <div>
+              <span class="text-red-500 text-xs italic">
+                Campo obligatorio.
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="-mx-3 md:flex mb-6">
+          <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="tel">
+              Número de teléfono*
+            </label>
+            <input class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1" style="color:black;" id="tel" type="number" name="tel" min="300" placeholder="Ej: 311585984">
+            <div>
+              <span class="text-red-500 text-xs italic">
+                Campo obligatorio.
+              </span>
+            </div>
+          </div>
+          <div class="md:w-1/2 px-3">
+            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="title">
+              Dirección de correspondencia*
+            </label>
+            <input class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1" style="color:black" id="title" type="text" placeholder="Ej: Carrera 11 #23-11">
+            <div>
+              <span class="text-red-500 text-xs italic">
+                Campo obligatorio.
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="-mx-3 md:flex mb-6">
+          <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="myDate">
+              Fecha de nacimiento*
+            </label>
+            <input class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1" style="color:black;" type="date" name="myDate" min="1900-01-01" max="2030-01-01" step="7" id="myDate" placeholder="Ej: 01/01/2000">
+            <div>
+              <span class="text-red-500 text-xs italic">
+                Campo obligatorio.
+              </span>
+            </div>
+          </div>
+          <div class="md:w-1/2 px-3">
+            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="title">
+              Lugar de nacimiento*
+            </label>
+            <input class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1" style="color:black" id="title" type="text" placeholder="Ej: Pereira/Risaralda">
+            <div>
+              <span class="text-red-500 text-xs italic">
+                Campo obligatorio.
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="-mx-3 md:flex mb-6">
+          <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="myDate">
+              Género*
+            </label>
+            <div>
+              <select class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1" style="color:black" id="title" type="text" placeholder="Ej: Masculino">
+                <option>Masculino</option>
+                <option>Femenino</option>
+                <option>Otro</option>
+              </select>
+            </div>
+            <div>
+              <span class="text-red-500 text-xs italic">
+                Campo obligatorio.
+              </span>
+            </div>
+          </div>
+          <div class="md:w-1/2 px-3">
+            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="title">
+              Temas literarios de preferencia*
+            </label>
+            <div>
+              <select class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1" style="color:black" id="title" type="text" placeholder="Ej: Suspenso">
+                <option>Aventuras</option>
+                <option>Ciencia Ficción</option>
+                <option>Terror y misterio</option>
+                <option>Romántica</option>
+                <option>Mitología</option>
+              </select>
+            </div>
+            <div>
+              <span class="text-red-500 text-xs italic">
+                Campo obligatorio.
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="-mx-3 md:flex mt-2">
+          <div class="md:w-full px-3 text-center">
+            <button class=" md:w-full font-bold py-2 px-4 border-b-4 hover:border-b-4 border-gray-500 hover:border-black rounded-full" style="background-color: #14b8a6; color:black;">
+              Registrate
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="grid grid-rows-2 p-2">
-      <p for="Gender">Generos literarios de su preferencia</p>
-      <div class="grid grid-cols-4">
-        <div>
-          <input type="checkbox" value="otro" id="checkbox-1" name="genero" />
-          <label for="checkbox">Otro</label>
-        </div>
-        <div>
-          <input type="checkbox" value="otro" id="checkbox-1" name="genero" />
-          <label for="checkbox">Otro</label>
-        </div>
-        <div>
-          <input type="checkbox" value="otro" id="checkbox-1" name="genero" />
-          <label for="checkbox">Otro</label>
-        </div>
-        <div>
-          <input type="checkbox" value="otro" id="checkbox-1" name="genero" />
-          <label for="checkbox">Otro</label>
-        </div>
-      </div>
-    </div>
-    <div id="btonRegis" class="text-center mt-6">
-      <button
-        class="text-black text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        style="background-color: rgb(20 184 166)" @click="login">
-        Registrate
-      </button>
-    </div>
+    </form>
   </div>
+  <!--<script>
+				const btn = document.querySelector("button.mobile-menu-button");
+				const menu = document.querySelector(".mobile-menu");
+
+				btn.addEventListener("click", () => {
+					menu.classList.toggle("hidden");
+				});
+	</script>-->
 </template>
+
 
 <script>
 export default {
@@ -108,46 +180,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#regLogo {
-  height: 9vw !important;
-}
-
-h1 {
-  text-align: center;
-  font-size: 2em !important;
-  color: black;
-  font-family: 'Abhaya Libre', serif;
-  border-bottom: 2px solid rgb(20 184 166);
-  margin-bottom: 2rem;
-}
-
-input [type='text'],
-[type='email'],
-[type='url'],
-[type='password'],
-[type='number'],
-[type='date'],
-[type='datetime-local'],
-[type='month'],
-[type='search'],
-[type='tel'],
-[type='time'],
-[type='week'],
-[multiple]{
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  background-color: #def2f1;
-  border-color: #3aafa9;
-  border-width: 1px;
-  border-radius: 10px;
-  padding-top: 0.5rem;
-  padding-right: 0.75rem;
-  padding-bottom: 0.5rem;
-  padding-left: 0.75rem;
-  font-size: 1rem;
-  line-height: 1.5rem;
-}
-</style>
