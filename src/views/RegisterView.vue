ba8f62e95d482eb0ef687f13bebc2b4becb5f969
JuanDaniel <daniel.angel@utp.edu.co>
2022-10-22 14:52:34 -0500

    mejora del registro

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -119,19 +119,11 @@
               </span>
             </div>
           </div>
-          <div class="md:w-1/2 px-3">
-            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="title">
-              Temas literarios de preferencia*
+          <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="company">
+              Contraseña*
             </label>
-            <div>
-              <select class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1" style="color:black" id="title" type="text" placeholder="Ej: Suspenso">
-                <option>Aventuras</option>
-                <option>Ciencia Ficción</option>
-                <option>Terror y misterio</option>
-                <option>Romántica</option>
-                <option>Mitología</option>
-              </select>
-            </div>
+            <input class="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-1" style="color:black;" id="company" type="password" placeholder="Ej: Pepito0102">
             <div>
               <span class="text-red-500 text-xs italic">
                 Campo obligatorio.
@@ -139,7 +131,50 @@
             </div>
           </div>
         </div>
-        <div class="-mx-3 md:flex mt-2">
+        <div class="-mx-3 mb-full">
+          <div>
+            <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="title">
+              Temas literarios de preferencia*
+            </label>
+            <ul class="items-center w-full text-sm font-medium rounded-lg sm:flex" style="background-color: #def2f1; border: 1px solid rgb(20 184 166);">
+                <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600" style="border-right: 1px solid rgb(20 184 166);">
+                    <div class="flex items-center pl-3">
+                        <input id="vue-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                        <label for="vue-checkbox-list" class="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray">Mitología</label>
+                    </div>
+                </li>
+                <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600" style="border-right: 1px solid rgb(20 184 166);">
+                    <div class="flex items-center pl-3">
+                        <input id="react-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                        <label for="react-checkbox-list" class="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray">Aventuras</label>
+                    </div>
+                </li>
+                <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600" style="border-right: 1px solid rgb(20 184 166);">
+                    <div class="flex items-center pl-3">
+                        <input id="angular-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                        <label for="angular-checkbox-list" class="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray">Ciencia Ficción</label>
+                    </div>
+                </li>
+                <li class="w-full dark:border-gray-600">
+                    <div class="flex items-center pl-3">
+                        <input id="laravel-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                        <label for="laravel-checkbox-list" class="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray">Terror y Misterio</label>
+                    </div>
+                </li>
+                <li class="w-full dark:border-gray-600">
+                    <div class="flex items-center pl-3">
+                        <input id="laravel-checkbox-list" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                        <label for="laravel-checkbox-list" class="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray">Romántica</label>
+                    </div>
+                </li>
+            </ul>
+
+              <span class="text-red-500 text-xs italic">
+                Campo obligatorio.
+              </span>
+          </div>
+        </div>
+        <div class="-mx-3 md:flex mt-9">
           <div class="md:w-full px-3 text-center">
             <button class=" md:w-full font-bold py-2 px-4 border-b-4 hover:border-b-4 border-gray-500 hover:border-black rounded-full" style="background-color: #14b8a6; color:black;">
               Registrate
