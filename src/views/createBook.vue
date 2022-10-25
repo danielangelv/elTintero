ee4e5238f2201f06ba92fd261785aac992bb1103
carolina <carolina.pelaez1@utp.edu.co>
2022-10-24 22:50:21 -0500

    punto de guardado en caso de emergencia

--- src/views/createBook.vue
+++ src/views/createBook.vue
@@ -1,128 +1,171 @@
 <template>
 
-    <form id="boxCrearLibro" class="container">
-        <div>
+    <div id="boxCrearLibro" class="container_crearLibro">
+
+        <div class="crear_libro">
             <h1>Crear Libro</h1>
-        </div>
-        <div id="interBox" class="row">
-        <div class="col-md-4">
-            <label for="Sinopsis" class="form-label">Escribe la sinopsis del Libro</label>
-            <input type="text" placeholder="Sinopsis del libro..." class="form-control" id="Sinopsis" />
+
         </div>
 
-        <h2>CARACTETISTICAS</h2>
-        <div id="caracteristicas" class="col-md-5">
-            <label for="AñoEdicion" class="form-label col-md-6">Ingresa el año de edición:</label>
-            <input type="number" placeholder="Ej: 2022" class="form-control" id="AñoEdicion" />
-        </div>
-        <div id="caracteristicas" class="col-md-5">
-            <label for="Tipo" class="form-label col-md-4">Ingresa el tipo:</label>
-            <input type="text" placeholder="Ej: Libro" class="form-control" id="Tipo" />
-        </div>
-        <div id="caracteristicas" class="col-md-5">
-            <label for="Autor" class="form-label col-md-4">Ingresa el autor:</label>
-            <input type="text" placeholder="Ej: Joël Dicker" class="form-control" id="Autor" />
-        </div>
-        <div id="caracteristicas" class="col-md-6">
-            <label for="ISXN" class="form-label col-md-3">Ingresa el ISXN:</label>
-            <input type="text" placeholder="Ej: 9786287525573" class="form-control" id="ISXN" />
-        </div>
-        <div id="caracteristicas" class="col-md-7">
-            <label for="idioma" class="form-label col-md-3">Ingresa el idioma:</label>
-            <input type="text" placeholder="Ej: Español" class="form-control" id="idioma" />
-        </div>
-        <div id="caracteristicas" class="col-md-7">
-            <label for="NumPaginas" class="form-label col-md-5">Ingresa el número de paginas:</label>
-            <input type="text" placeholder="Ej: 592" class="form-control" id="NumPaginas" />
-        </div>
-        <div id="caracteristicas" class="col-md-7">
-            <label for="Peso" class="form-label col-md-4">Ingresa el peso (Fisico):</label>
-            <input type="text" placeholder="Ej: 610" class="form-control" id="Peso" />
-        </div>
-        <div id="caracteristicas" class="col-md-7">
-            <label for="Tamaño" class="form-label col-md-5">Ingresa el tamaño (Fisico):</label>
-            <input type="text" placeholder="Ej: 15 x 24 cm" class="form-control" id="Tamaño" />
-        </div>
-        <div id="caracteristicas" class="col-md-7">
-            <label for="Acabado" class="form-label col-md-5">Ingresa el acabado (Fisico):</label>
-            <input type="text" placeholder="Ej: Tapa Rústica" class="form-control" id="Acabado" />
-        </div>
-        <div id="caracteristicas" class="col-md-7">
-            <label for="Titulo" class="form-label col-md-3">Ingresa el titulo:</label>
-            <input type="text" placeholder="Ej: El caso de Alaska Sanders" class="form-control" id="Titulo" />
-        </div>
-      </div>
-    </form>
+        <div class="row  pl-3">
 
+            <div>
+                <label for="Sinopsis" class="form-label">Sinopsis: </label><br>
+                <input type="text" placeholder="Sinopsis del libro..." class="text-xs" id="Sinopsis" />
+                <div>
+                </div>
+                <h2>CARACTETISTICAS</h2>
 
+                <div id="caracteristicas">
+                    <label for="AñoEdicion">Ingresa el año de edición:</label>
+                    <input type="number" placeholder="Ej: 2022" class="text-xs" id="AñoEdicion" />
+                </div>
+
+                <div id="caracteristicas">
+                    <label for="Tipo">Ingresa el tipo:</label>
+                    <input type="text" placeholder="Ej: Libro" class="text-xs" id="Tipo" />
+                </div>
+
+                <div id="caracteristicas">
+                    <label for="Autor">Ingresa el autor:</label>
+                    <input type="text" placeholder="Ej: Joël Dicker" class="text-xs" id="Autor" />
+                </div>
+
+                <div id="caracteristicas">
+                    <label for="ISXN">Ingresa el ISXN:</label>
+                    <input type="text" placeholder="Ej: 9786287525573" class="text-xs" id="ISXN" />
+                </div>
+
+                <div id="caracteristicas">
+                    <label for="idioma">Ingresa el idioma:</label>
+                    <input type="text" placeholder="Ej: Español" class="text-xs" id="idioma" />
+                </div>
+
+                <div id="caracteristicas">
+                    <label for="NumPaginas">Ingresa el número de paginas:</label>
+                    <input type="text" placeholder="Ej: 592" class="text-xs" id="NumPaginas" />
+                </div>
+
+                <div id="caracteristicas">
+                    <label for="Peso">Ingresa el peso (Fisico):</label>
+                    <input type="text" placeholder="Ej: 610" class="text-xs" id="Peso" />
+                </div>
+
+                <div id="caracteristicas">
+                    <label for="Tamaño">Ingresa el tamaño (Fisico):</label>
+                    <input type="text" placeholder="Ej: 15 x 24 cm" class="text-xs" id="Tamaño" />
+                </div>
+
+                <div id="caracteristicas">
+                    <label for="Acabado">Ingresa el acabado (Fisico):</label>
+                    <input type="text" placeholder="Ej: Tapa Rústica" class="text-xs" id="Acabado" />
+                </div>
+
+                <div id="caracteristicas">
+                    <label for="Titulo">Ingresa el titulo:</label>
+                    <input type="text" placeholder="Ej: El caso de Alaska Sanders" class="text-xs" id="Titulo" />
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="subir_img  pl-5">
+            <label for="Sinopsis" class="form-label">Portada: </label><br><br>
+            <input type="file" id="seleccionArchivos" accept="image/*" @change="subir_img">
+            <br><br>
+            <img id="imagenPrevisualizacion">
+
+        </div>
+
+        <div class="row_end  pl-5">
+            <div>
+                <label for="Titulo">Titulo:</label>
+                <input type="text" class="form-control" id="Titulo" />
+            </div>
+            <br><br>
+            <div>
+                <label for="Editorial">Editorial:</label>
+                <input type="text" class="form-control" id="Editorial" /><br>
+            </div>
+            <br><br>
+            <div>
+                <label for="Autor">Ingresa el autor:</label>
+                <input type="text" placeholder="Ej: Joël Dicker" class="form-control" id="Autor" /><br>
+            </div>
+            <br><br>
+            <div>
+                <form action="/action_page.php">
+                    <label for="quantity">Ingresa la cantidad de libros disponibles: </label><br>
+                    <input type="number" id="quantity" name="quantity" min="0" max="20">
+                </form>
+            </div>
+        </div>
+
+    </div>
 
 </template>
 
-<style>
+<script>
+export default {
+    methods: {
+        subir_img: function (e) {
+            console.log (e);
+            const seleccionArchivos = document.querySelector("#seleccionArchivos");
+            const imagenPrevisualizacion = document.querySelector("#imagenPrevisualizacion");
+            const archivos = seleccionArchivos.files;
+            if (!archivos || !archivos.length) {
+                imagenPrevisualizacion.src = "";
+                return;
+            }
+            const primerArchivo = archivos[0];
+            const objectURL = URL.createObjectURL(primerArchivo);
+            imagenPrevisualizacion.src = objectURL;
+        }
+    }
+}
 
-#boxCrearLibro
-{
+</script>
+
+<style>
+#boxCrearLibro {
     color: black;
     background-color: white;
 }
 
-#AñoEdicion
-{
-    border: none;
-}
-
-h1
-{
-  text-align: center;
-  font-size: 2em !important;
-  color: black;
-  font-family: 'Abhaya Libre', serif;
-  border-bottom: 2px solid rgb(20 184 166);
-  margin-bottom: 2rem;
-}
-
-h2
-{
-    padding-top: 40px;
-    padding-bottom: 0px;
-}
-
-[type='text']
-{
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-    background-color: #ffffff;
-    border: none;
-    font-weight:1000;
-}
-
-#interBox
-{
-    
-    --bs-gutter-x: 1.5rem;
-    --bs-gutter-y: 0;
-    display: flex;
-    flex-wrap: wrap;
-    margin-top: calc(-1 * var(--bs-gutter-y));
-    margin-right: calc(-.5 * var(--bs-gutter-x));
-    margin-left: calc(-.5 * var(--bs-gutter-x));
-    margin-bottom: 10px;
-    font-family: 'Roboto Mono', monospace;
-    color: black;
-    flex-direction: column;
+.container_crearLibro {
+    display: grid;
 
 }
 
-#caracteristicas
-{
-    margin-top: 10px;
+.container_crearLibro div {
+    grid-template-columns: auto auto auto;
+    grid-template-rows: auto auto auto;
+}
+
+.crear_libro {
+    grid-column: 1 / span 3;
+}
+
+.row {
+    grid-column: 1 / span 1;
+    grid-row: 2 / span 2;
+}
+
+.ubir_img {
+    grid-column: 2 / span 1;
+    grid-row: 2 / span 2;
+}
+
+.row_end {
+    grid-column: 3 / span 1;
+    grid-row: 2 / span 2;
+}
+
+/*#boxCrearLibro {
     display: flex;
     flex-direction: row;
     flex-wrap: nowrap;
-    align-content: space-between;
-    justify-content: space-between;
-    align-items: center;
-}
-
+    justify-content: space-evenly;
+}*/
 </style>
