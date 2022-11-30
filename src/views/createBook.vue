bdbbd30c037e3b6591bb28762b95478fb1cdf98a
root <root@JAIME_LEANWARE.localdomain>
2022-11-30 09:23:00 -0500

    fix conflicts

--- src/views/createBook.vue
+++ src/views/createBook.vue
@@ -154,7 +154,171 @@
                 class="font-bold py-2 px-4 border-b-4 hover:border-b-4 border-gray-500 hover:border-black rounded-full"
                 style="background-color: #14b8a6; color:black;" @click="crear">Crear</button>
         </div>
+        <div class="grid grid-cols-2 gap-1">
+          <label for="">Idioma</label>
+          <input
+            v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')"
+            v-model="language"
+            type="text"
+            class="w-full"
+            v-bind:disabled="
+              !(this.user && this.user.auth_helper.includes('CRUD_BOOKS'))
+            "
+          />
+          <span v-else>{{ language }}</span>
+        </div>
+        <div class="grid grid-cols-2 gap-1">
+          <label for="">Num Paginas</label>
+          <input
+            v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')"
+            v-model="num_pag"
+            type="number"
+            min="1"
+            class="w-full"
+            v-bind:disabled="
+              !(this.user && this.user.auth_helper.includes('CRUD_BOOKS'))
+            "
+          />
+          <span v-else>{{ num - pag }}</span>
+        </div>
+        <div class="grid grid-cols-2 gap-1">
+          <label for="">Peso</label
+          ><input
+            v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')"
+            v-model="weight"
+            type="number"
+            class="w-full"
+            v-bind:disabled="
+              !(this.user && this.user.auth_helper.includes('CRUD_BOOKS'))
+            "
+          /><span v-else>{{ weight }}</span>
+        </div>
+        <div class="grid grid-cols-2 gap-1">
+          <label for="">Tamaño</label>
+          <input
+            v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')"
+            v-model="size"
+            type="text"
+            class="w-full"
+            v-bind:disabled="
+              !(this.user && this.user.auth_helper.includes('CRUD_BOOKS'))
+            "
+          />
+          <span v-else>{{ size }}</span>
+        </div>
+        <div class="grid grid-cols-2 gap-1">
+          <label for="">Acabado</label>
+          <input
+            v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')"
+            v-model="acabado"
+            type="text"
+            class="w-full"
+            v-bind:disabled="
+              !(this.user && this.user.auth_helper.includes('CRUD_BOOKS'))
+            "
+          />
+          <span v-else>{{ acabado }}</span>
+        </div>
+        <FormTopics
+          ref="topic_ref"
+          v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')"
+        ></FormTopics>
+      </div>
+      <div>
+        <h3 v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')">
+          portada
+        </h3>
+        <input
+          v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')"
+          type="file"
+          id="seleccionArchivos"
+          accept="image/*"
+          @change="subir_img"
+        />
+        <img id="imagenPrevisualizacion" />
+      </div>
+      <div>
+        <div class="grid grid-cols-2">
+          <label>Nombre</label
+          ><input
+            v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')"
+            v-model="nombre"
+            class="w-full"
+            type="text"
+            v-bind:disabled="
+              !(this.user && this.user.auth_helper.includes('CRUD_BOOKS'))
+            "
+          /><span v-else>{{ acabado }}</span>
+        </div>
+        <div class="grid grid-cols-2">
+          <label>Autor</label
+          ><input
+            v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')"
+            v-model="autor"
+            type="text"
+            class="w-full"
+            v-bind:disabled="
+              !(this.user && this.user.auth_helper.includes('CRUD_BOOKS'))
+            "
+          /><span v-else>{{ acabado }}</span>
+        </div>
+        <div class="grid grid-cols-2">
+          <label>Editorial</label
+          ><input
+            v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')"
+            v-model="editorial"
+            type="text"
+            class="w-full"
+            v-bind:disabled="
+              !(this.user && this.user.auth_helper.includes('CRUD_BOOKS'))
+            "
+          /><span v-else>{{ acabado }}</span>
+        </div>
+        <div class="grid grid-cols-2">
+          <label>Precio en pesos</label
+          ><input
+            v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')"
+            v-model="price"
+            type="text"
+            class="w-full"
+            v-bind:disabled="
+              !(this.user && this.user.auth_helper.includes('CRUD_BOOKS'))
+            "
+          /><span v-else>{{ acabado }}</span>
+        </div>
+        <div class="grid grid-cols-2">
+          <label>Ingrese la cantidad de libros disponibles:</label>
+          <input
+            v-if="this.user && this.user.auth_helper.includes('CRUD_BOOKS')"
+            v-model="number"
+            type="number"
+            min="1"
+            v-bind:disabled="
+              !(this.user && this.user.auth_helper.includes('CRUD_BOOKS'))
+            "
+          /><span v-else>{{ acabado }}</span>
+        </div>
+      </div>
     </div>
+    <div class="text-center items-center content-center">
+      <button
+        v-if="$route.params.id"
+        class="font-bold py-2 px-4 border-b-4 hover:border-b-4 border-gray-500 hover:border-black rounded-full"
+        style="background-color: #14b8a6; color: black"
+        @click="actualizar"
+      >
+        Actualizar
+      </button>
+      <button
+        v-else
+        class="font-bold py-2 px-4 border-b-4 hover:border-b-4 border-gray-500 hover:border-black rounded-full"
+        style="background-color: #14b8a6; color: black"
+        @click="crear"
+      >
+        Crear
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -192,7 +356,7 @@ export default {
                 const answer = await response.json();
                 this.id = answer.book.id
                 this.price = answer.book.price
-                this.name = answer.book.name
+                this.nombre = answer.book.name
                 this.editorial = answer.book.editorial
                 this.number = answer.book.quantity
                 this.acabado = answer.book.acabado
@@ -202,13 +366,14 @@ export default {
                 this.date = answer.book.publication_date
                 this.autor = answer.book.autor
                 this.num_pag = answer.book.number_pages
+                this.language = answer.book.idioma
+                this.weight = answer.book.weight
                 console.log(answer)
                 const imagenPrevisualizacion = document.querySelector("#imagenPrevisualizacion");
                 imagenPrevisualizacion.src = answer.book.img
 
             }
             else {
-                //alert("No se pudo crear el libro intente nuevamente")
                 snackBar.showSnackBar("No se pudo crear el libro, intenta nuevamente")
             }
             console.log("codigo")
@@ -230,15 +395,7 @@ export default {
             const objectURL = URL.createObjectURL(primerArchivo);
             imagenPrevisualizacion.src = objectURL;
         },
-        sum: function (e) {
-            console.log(e)
-            this.number = this.number + 1
-        },
-        minus: function (e) {
-            console.log(e)
-            this.number = this.number - 1
-        },
-        read_file: async function () {
+        read_file: async function() {
             return new Promise((resolve, reject) => {
                 const seleccionArchivos = document.querySelector("#seleccionArchivos");
                 const reader = new FileReader();
@@ -263,8 +420,8 @@ export default {
                 "number": this.number,
                 "price": this.price,
                 "topics": this.$refs.topic_ref.choseen_topics,
-                "image": await this.read_file(),
-                "weigh": this.weight
+                "image":await this.read_file(),
+                "weight":this.weight
             }
             const requestOptions = {
                 method: "POST",
@@ -321,7 +478,6 @@ export default {
             }
     }
 }
-
 </script>
 
 <style>
@@ -332,8 +488,8 @@ h2 {
 }
 
 #boxCrearLibro {
-    color: black;
-    background-color: white;
+  color: black;
+  background-color: white;
 }
 
 label {
